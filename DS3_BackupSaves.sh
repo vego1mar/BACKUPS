254df340ec2da1d9932791203ec5bf2b9bb42665
Paweł Siciński <koryfeusz.ksiezyca@gmail.com>
2019-04-19 12:26:38 +0200

    Backup script altered.

--- DS3_BackupSaves.sh
+++ DS3_BackupSaves.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
-# This script is designed to use it with Linux shell.
+#!/bin/echo
+#!/usr/bin/7z
 
-RED_COLOR='\033[0;31m'
-GREEN_COLOR='\033[0;32m'
-NO_COLOR='\033[0m'
-ARCHIVER_EXEC=/usr/bin/7z
 GAMESAVE_DIR="/media/moon/26509ADF509AB4D1/Users/Mar/AppData/Roaming/DarkSoulsIII"
-ARCHIVE_PATH=$HOME"/Documents/Projekty/BACKUPS/Dark_Souls_III__AppData_SL${1}.7z"
+ARCHIVE_PATH="$HOME""/Documents/Projekty/BACKUPS/Dark_Souls_III__AppData_SL${1}.7z"
 
-if [ ! -f "$ARCHIVER_EXEC" ]; then
-  echo -e "${RED_COLOR}Archiver program does not exist within provided path.${NO_COLOR}"
-  exit 1
-fi
+function check_game_save_directory() {
+    local saves_dir="$1"
 
-if [ ! -d "$GAMESAVE_DIR" ]; then
-  echo -e "${RED_COLOR}Gamesave directory does not exist within provided path.${NO_COLOR}"
-  exit 1
-fi
+    if [ ! -d "$saves_dir" ]; then
+        print_in_red "Game save directory does not exist!"
+        exit 1
+    fi
+}
 
-echo -e "${GREEN_COLOR}Archiving Dark Souls III gamesaves...${NO_COLOR}"
-"$ARCHIVER_EXEC" a -t7z -mx=9 -m0=lzma -md=128m -mfb=273 -mmt=4 -ms=on "$ARCHIVE_PATH" "${GAMESAVE_DIR}"
-echo -e "${GREEN_COLOR}Script done.${NO_COLOR}"
+function archive_game_saves() {
+    local source="$1"
+    local target="$2"
+    local evaluate=$(7z a -t7z -mx=9 -m0=lzma -md=128m -mfb=273 -mmt=4 -ms=on "$target" "$source")
+}
+
+function print_in_green() {
+    local text="$1"
+    local green='\033[0;32m'
+    local no_color='\033[0m'
+    echo -e "${green}$text${no_color}"
+}
+
+function print_in_red() {
+    local text="$1"
+    local red='\033[0;31m'
+    local no_color='\033[0m'
+    echo -e "${red}$text${no_color}"
+}
+
+
+function __main() {
+    check_game_save_directory "$GAMESAVE_DIR"
+    print_in_green "Archiving Dark Souls III gamesaves..."
+    archive_game_saves "$GAMESAVE_DIR" "$ARCHIVE_PATH"
+    print_in_green "Script done."
+}
+
+
+__main "$@"
